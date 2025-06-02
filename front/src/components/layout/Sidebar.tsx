be4852f153d088a3a0153810d0b377b4a1646dee
Loading <bfx3.1415@gmail.com>
2025-06-02 19:47:58 -0400

    fix: rm Select XML Files button

--- front/src/components/layout/Sidebar.tsx
+++ front/src/components/layout/Sidebar.tsx
@@ -207,12 +207,12 @@ const Sidebar: React.FC = () => {
         )}
       </div>
       
-      <div className="p-3 border-t border-neutral-200">
+      {/* <div className="p-3 border-t border-neutral-200">
         <button className="w-full flex items-center justify-center px-4 py-2 bg-primary-700 hover:bg-primary-800 text-white rounded-md transition-colors">
           <FolderOpen className="h-4 w-4 mr-2" />
           <span className="text-sm font-medium">Select XML Files</span>
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
